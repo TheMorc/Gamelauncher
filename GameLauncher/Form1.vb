0422a0440e1e20e4c411bdce74ef510fb3a24496
TheMorc <r.gracik@gmail.com>
2017-02-11 22:45:47 +0100

    added simple version check

--- GameLauncher/Form1.vb
+++ GameLauncher/Form1.vb
@@ -1,4 +1,7 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.Net
+
+Public Class Form1
     Dim verzia As String = 0.5
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If My.Settings.nonanimode = True Then
@@ -7,12 +10,17 @@
         Else
             settings.CheckBox1.Checked = False
         End If
-
-        dialog.ShowDialog()
+        Dim address As String = "https://github.com/TheMorc/Gamelauncher/blob/master/GameLauncher/version.txt"
+        Dim client As WebClient = New WebClient()
+        Dim reader As StreamReader = New StreamReader(client.OpenRead(address))
+        If reader.ReadToEnd IsNot verzia Then
+            dialog.Label1.Text = "Uh oh! There's a new version availible"
+            dialog.ShowDialog()
+        End If
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        settings.show()
+        settings.Show()
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
