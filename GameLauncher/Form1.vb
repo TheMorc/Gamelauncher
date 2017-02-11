9de0548560592d23345ef4a4b971d4449be46565
TheMorc <r.gracik@gmail.com>
2017-02-11 22:52:55 +0100

    fixed checking, should work as expected

--- GameLauncher/Form1.vb
+++ GameLauncher/Form1.vb
@@ -2,7 +2,9 @@
 Imports System.Net
 
 Public Class Form1
+    Dim newest As Boolean
     Dim verzia As String = 0.5
+    Dim newestver As String
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If My.Settings.nonanimode = True Then
 
@@ -10,11 +12,15 @@ Public Class Form1
         Else
             settings.CheckBox1.Checked = False
         End If
-        Dim address As String = "https://github.com/TheMorc/Gamelauncher/blob/master/GameLauncher/version.txt"
+        Dim address As String = "https://raw.githubusercontent.com/TheMorc/Gamelauncher/master/GameLauncher/version.txt"
         Dim client As WebClient = New WebClient()
         Dim reader As StreamReader = New StreamReader(client.OpenRead(address))
-        If reader.ReadToEnd IsNot verzia Then
-            dialog.Label1.Text = "Uh oh! There's a new version availible"
+        newestver = reader.ReadToEnd
+        If newestver = verzia Then
+            newest = True
+        Else
+            newest = False
+            dialog.Label1.Text = "Uh oh! There's a new version available " + newestver
             dialog.ShowDialog()
         End If
     End Sub
@@ -24,6 +30,6 @@ Public Class Form1
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        Label1.Text = My.Settings.nonanimode.ToString + vbNewLine + Width.ToString + "x" + Height.ToString + vbNewLine + verzia
+        Label1.Text = My.Settings.nonanimode.ToString + vbNewLine + Width.ToString + "x" + Height.ToString + vbNewLine + verzia + vbNewLine + newest.ToString + " " + newestver
     End Sub
 End Class
